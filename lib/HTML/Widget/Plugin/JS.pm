a8bf935e7f07f87d01472412f4863694cd6e0cca
Ricardo SIGNES <rjbs@cpan.org>
2014-06-03 12:40:56 +0000

    Import of RJBS/HTML-Widget-Plugin-JS-0.005 from CPAN.
    gitpan-cpan-distribution: HTML-Widget-Plugin-JS
    gitpan-cpan-version:      0.005
    gitpan-cpan-path:         RJBS/HTML-Widget-Plugin-JS-0.005.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/HTML/Widget/Plugin/JS.pm
+++ lib/HTML/Widget/Plugin/JS.pm
@@ -1,11 +1,9 @@
 use strict;
 use warnings;
 package HTML::Widget::Plugin::JS;
-{
-  $HTML::Widget::Plugin::JS::VERSION = '0.004';
-}
-use parent qw(HTML::Widget::Plugin);
 # ABSTRACT: a JavaScript variable declaration emitter
+$HTML::Widget::Plugin::JS::VERSION = '0.005';
+use parent qw(HTML::Widget::Plugin);
 
 use Data::JavaScript::Anon;
 
@@ -14,6 +12,26 @@ sub provided_widgets { qw(js_var js_vars js_anon) }
 sub boolean_args {}
 sub attribute_args {}
 
+#pod =head2 js_var
+#pod
+#pod =head2 js_vars
+#pod
+#pod These are two names for the same widget.  Given a hashref, they will produce
+#pod JavaScript code to assign the data in the hashref.
+#pod
+#pod In otherwords, this widget:
+#pod
+#pod   $fac->js_vars({
+#pod     foo => { a => 1, b => 2 },
+#pod     bar => [ 4, 2, 3 ],
+#pod   });
+#pod
+#pod ...will be rendered something like this:
+#pod
+#pod   var foo = { a: 1, b: 2 };
+#pod   var bar = [ 1, 2, 3 ];
+#pod
+#pod =cut
 
 sub js_vars {
   my ($self, $factory, $arg) = @_;
@@ -28,6 +46,13 @@ sub js_vars {
 
 BEGIN { *js_var = \&js_vars }
 
+#pod =head2 js_anon
+#pod
+#pod This widget converts a given data structure to an anonymous JavaScript
+#pod structure.  This basically just provides a widget factory interface to
+#pod Data::JavaScript::Anon.
+#pod
+#pod =cut
 
 sub js_anon {
   my ($self, $factory, $arg) = @_;
@@ -41,13 +66,15 @@ __END__
 
 =pod
 
+=encoding UTF-8
+
 =head1 NAME
 
 HTML::Widget::Plugin::JS - a JavaScript variable declaration emitter
 
 =head1 VERSION
 
-version 0.004
+version 0.005
 
 =head2 js_var
 
